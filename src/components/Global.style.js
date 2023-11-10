15bb79b280a0575e464921dce127fe5c85e5758e
Bogdan <bogdantisenko06@gmail.com>
2023-11-10 15:26:01 +0200

    minStyle

--- src/components/Global.style.js
+++ src/components/Global.style.js
@@ -3,6 +3,7 @@ import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
 body {
+  padding: 20px;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -14,5 +15,10 @@ body {
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
+}
+
+button {
+  margin-right: 8px;
+  cursor: pointer;
 }
 `;
