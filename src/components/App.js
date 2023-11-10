6e106e3bd5bbc48690c7260aa056f289a5839cfe
Bogdan <bogdantisenko06@gmail.com>
2023-11-10 13:37:04 +0200

    firstStep

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,47 @@
 import { GlobalStyle } from 'components/Global.style';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-      <GlobalStyle />
-    </div>
-  );
+const Button = ({ updateClics, buttonText }) => {
+  return <button onClick={updateClics}>{buttonText}</button>;
 };
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  updateNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  updateBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <Button updateClics={this.updateGood} buttonText="Good" />
+        <Button updateClics={this.updateNeutral} buttonText="Neutral" />
+        <Button updateClics={this.updateBad} buttonText="Bad" />
+        <h2>Statistic</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
