304592bd707dbc30d98e39b87513f6383c8b238c
Bogdan <bogdantisenko06@gmail.com>
2023-11-10 14:59:52 +0200

    secondStep

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import { GlobalStyle } from 'components/Global.style';
 import { Component } from 'react';
+import { Statistics } from './Statistic';
 
-const Button = ({ updateClics, buttonText }) => {
-  return <button onClick={updateClics}>{buttonText}</button>;
+export const Button = ({ updateClics, buttonText }) => {
+  return (
+    <button onClick={() => updateClics(buttonText.toLowerCase())}>
+      {buttonText}
+    </button>
+  );
 };
 
 export class App extends Component {
@@ -12,33 +17,39 @@ export class App extends Component {
     bad: 0,
   };
 
-  updateGood = () => {
+  updateFeedback = type => {
     this.setState(prevState => {
-      return { good: prevState.good + 1 };
-    });
-  };
-  updateNeutral = () => {
-    this.setState(prevState => {
-      return { neutral: prevState.neutral + 1 };
-    });
-  };
-  updateBad = () => {
-    this.setState(prevState => {
-      return { bad: prevState.bad + 1 };
+      return { [type]: prevState[type] + 1 };
     });
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const positive = this.state.good;
+    const percentage = total === 0 ? 0 : (positive / total) * 100;
+    return percentage.toFixed(0);
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <div>
         <h2>Please leave feedback</h2>
-        <Button updateClics={this.updateGood} buttonText="Good" />
-        <Button updateClics={this.updateNeutral} buttonText="Neutral" />
-        <Button updateClics={this.updateBad} buttonText="Bad" />
-        <h2>Statistic</h2>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
+        <Button updateClics={this.updateFeedback} buttonText="Good" />
+        <Button updateClics={this.updateFeedback} buttonText="Neutral" />
+        <Button updateClics={this.updateFeedback} buttonText="Bad" />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        ></Statistics>
 
         <GlobalStyle />
       </div>
